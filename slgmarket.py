60a4c4aeb00d981654f5ab63a554de58d982b8a6
ewhittley <eric.whittley@gmail.com>
2017-06-17 09:27:32 -0500

    Add Discount class

--- slgmarket.py
+++ slgmarket.py
@@ -23,6 +23,58 @@ class Product(object):
 	def update_product(self):
 		pass
 
+	def get_product(self, code=None):
+		pickle_file = open('product.pkl', 'rb')
+
+		pickle_data = pickle.load(pickle_file)
+
+		print(pickle_file)
+
+		pickle_file.close()
+
+
+class Discount(object):
+
+	def __init__(self, code, description, limit=None, 
+					from_product=None, from_quantity=None, 
+					to_product=None, to_quantity=None, amount=None):
+		self.code = code
+		self.description = description
+		self.limit = limit
+		self.from_product = from_product
+		self.from_quantity = from_quantity
+		self.to_product = to_product
+		self.to_quantity = to_quantity
+		self.amount = amount
+
+	def add_discount(self):
+		discount_data = {'code' : self.code,
+							'description' : self.description,
+							'limit' : self.limit,
+							'from_product' : self.from_product,
+							'from_quantity' : self.from_quantity,
+							'to_product' : self.to_product,
+							'to_quantity' : self.to_quantity,
+							'amount' : self.amount}
+
+		pickle_file = open('discount.pkl', 'wb')
+
+		pickle.dump(discount_data, pickle_file)
+
+		pickle_file.close()
+
+	def update_discount(self):
+		pass
+
+	def get_discount(self, code=None):
+		pickle_file = open('discount.pkl', 'rb')
+
+		pickle_data = pickle.load(pickle_file)
+
+		print(pickle_file)
+
+		pickle_file.close()
+
 
 if __name__ == "__main__":
 	action = raw_input("What would you like to do?\n- Add Product\n- Add Basket Item\n")
@@ -36,10 +88,4 @@ if __name__ == "__main__":
 
 		product.add_product()
 
-		pickle_file = open('product.pkl', 'rb')
-
-		pickle_data = pickle.load(pickle_file)
-
-		print(pickle_data)
-
-		pickle_file.close()
+		product.get_product()
