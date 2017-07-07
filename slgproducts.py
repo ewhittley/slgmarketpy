9572ad5b5b98fa694193515215e5e78f8c92c534
ewhittley <eric.whittley@gmail.com>
2017-07-06 21:34:51 -0500

    remove unnecessary self declares and update method

--- slgproducts.py
+++ slgproducts.py
@@ -4,20 +4,13 @@ import json_helper
 class Product(object):
 
     def add_product(self, code, name, price):
-        self.code = code
-        self.name = name
-        self.price = price
-
         product_data = {
-            'code': self.code,
-            'name': self.name,
-            'price': self.price
+            'code': code,
+            'name': name,
+            'price': price
             }
 
         json_helper.write_list('products.json', 'products', product_data)
-
-    def update_product(self, code, name=None, price=None):
-        pass
 
     def get_products(self, code=None):
         products = json_helper.get_list('products.json', 'code', code)
