473f21d09ad746f39a313c644389a07512a371c3
ewhittley <eric.whittley@gmail.com>
2017-07-11 09:43:00 -0500

    clean up test script

--- test_slgmarket.py
+++ test_slgmarket.py
@@ -6,72 +6,72 @@ import slgbasketitems
 class BasketItemsTester(unittest.TestCase):
 
     def test_chmk_discount(self):
-        self.basketitem = slgbasketitems.BasketItem()
+        basketitem = slgbasketitems.BasketItem()
 
-        self.products = ['CH1', 'AP1', 'CF1', 'MK1']
+        products = ['CH1', 'AP1', 'CF1', 'MK1']
 
-        for product in self.products:
+        for product in products:
             slgmarket.add_product_to_basket(product)
 
-        self.basket_items = self.basketitem.get_basket_items()
+        basket_items = basketitem.get_basket_items()
         # print(self.basket_items)
 
-        self.total = slgmarket.total_basket(self.basket_items)
+        total = slgmarket.total_basket(basket_items)
 
-        self.basketitem.destroy_basket_items()
+        basketitem.destroy_basket_items()
 
-        self.assertEqual(self.total, 20.34)
+        self.assertEqual(total, 20.34)
 
     def test_no_discount(self):
-        self.basketitem = slgbasketitems.BasketItem()
+        basketitem = slgbasketitems.BasketItem()
 
-        self.products = ['MK1', 'AP1']
+        products = ['MK1', 'AP1']
 
-        for product in self.products:
+        for product in products:
             slgmarket.add_product_to_basket(product)
 
-        self.basket_items = self.basketitem.get_basket_items()
+        basket_items = basketitem.get_basket_items()
         # print(self.basket_items)
 
-        self.total = slgmarket.total_basket(self.basket_items)
+        total = slgmarket.total_basket(basket_items)
 
-        self.basketitem.destroy_basket_items()
+        basketitem.destroy_basket_items()
 
-        self.assertEqual(self.total, 10.75)
+        self.assertEqual(total, 10.75)
 
     def test_bogo_discount(self):
-        self.basketitem = slgbasketitems.BasketItem()
+        basketitem = slgbasketitems.BasketItem()
 
-        self.products = ['CF1', 'CF1']
+        products = ['CF1', 'CF1']
 
-        for product in self.products:
+        for product in products:
             slgmarket.add_product_to_basket(product)
 
-        self.basket_items = self.basketitem.get_basket_items()
+        basket_items = basketitem.get_basket_items()
         # print(self.basket_items)
 
-        self.total = slgmarket.total_basket(self.basket_items)
+        total = slgmarket.total_basket(basket_items)
 
-        self.basketitem.destroy_basket_items()
+        basketitem.destroy_basket_items()
 
-        self.assertEqual(self.total, 11.23)
+        self.assertEqual(total, 11.23)
 
     def test_appl_discount(self):
-        self.basketitem = slgbasketitems.BasketItem()
+        basketitem = slgbasketitems.BasketItem()
 
-        self.products = ['AP1', 'AP1', 'CH1', 'AP1']
+        products = ['AP1', 'AP1', 'CH1', 'AP1']
 
-        for product in self.products:
+        for product in products:
             slgmarket.add_product_to_basket(product)
 
-        self.basket_items = self.basketitem.get_basket_items()
+        basket_items = basketitem.get_basket_items()
         # print(self.basket_items)
 
-        self.total = slgmarket.total_basket(self.basket_items)
+        total = slgmarket.total_basket(basket_items)
 
-        self.basketitem.destroy_basket_items()
+        basketitem.destroy_basket_items()
 
-        self.assertEqual(self.total, 16.61)
+        self.assertEqual(total, 16.61)
 
 
 def main():
